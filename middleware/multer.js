ba05c3d481776370b34d290066a1a310e1e4655a
fedechaves <federicochavesf@gmail.com>
2023-01-08 20:53:11 -0300

    quick footer fix on albums, multer error on going

--- middleware/multer.js
+++ middleware/multer.js
@@ -4,8 +4,10 @@ const path = require("path");
 module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
+    const validationErrors = [];
     let ext = path.extname(file.originalname);
     if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+      validationErrors.push({ msg: "File type is not supported" })
       cb(new Error("File type is not supported"), false);
       return;
     }
